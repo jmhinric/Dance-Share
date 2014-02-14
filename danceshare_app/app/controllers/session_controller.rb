ec3d5dd5986a7a5fcd5580552f4bf58b63a88742
John Hinrichs <jmhinric@gmail.com>
2014-02-14 15:47:59 -0500

    User can sign out

--- danceshare_app/app/controllers/session_controller.rb
+++ danceshare_app/app/controllers/session_controller.rb
@@ -5,7 +5,7 @@ class SessionController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    
+
     if  user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user_path(user)
@@ -16,7 +16,8 @@ class SessionController < ApplicationController
   end
 
   def destroy
-
+    session[:user_id] = nil
+    redirect_to root_path
   end
 
 end
