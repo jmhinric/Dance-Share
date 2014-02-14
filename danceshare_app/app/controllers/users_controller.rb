130ba81c72504936a87cdab317e02d9cc811441f
John Hinrichs <jmhinric@gmail.com>
2014-02-14 16:36:54 -0500

    Only valid user can edit and destroy their account

--- danceshare_app/app/controllers/users_controller.rb
+++ danceshare_app/app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
 
   before_action :load_user, only: [:show, :edit, :update, :destroy]
 
+  before_action :authenticate, :authorize, only: [:edit, :update, :destroy]
+
   def show
   end
 
@@ -43,6 +45,20 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :gender, :dob, :photo_url, :created_at, :updated_at)
+    end
+
+    def authenticate
+      unless logged_in?
+        redirect_to login_path
+      end
+    end
+
+    def authorize
+      if current_user != @user && !logged_in?
+        redirect_to login_path
+      elsif current_user != @user
+        redirect_to root_path
+      end
     end
 
 end
