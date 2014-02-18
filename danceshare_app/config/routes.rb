33f3bddf4ad40ef57043f6b5e2fceb78a76af162
John Hinrichs <jmhinric@gmail.com>
2014-02-18 09:21:18 -0500

    refactor routes

--- danceshare_app/config/routes.rb
+++ danceshare_app/config/routes.rb
@@ -16,11 +16,11 @@ DanceshareApp::Application.routes.draw do
   post "/session", to: "session#create"
   delete "/session", to: "session#destroy"
 
-  # get "/users/:id/reviews", to: "users#reviews_index"
-  get "/users/:id/reviews", to: "reviews#index"
-  get "/users/:user_id/reviews/:id", to: "reviews#show"
+  # get "/users/:id/reviews", to: "reviews#index"
+  # get "/users/:user_id/reviews/:id", to: "reviews#show"
 
   resources :users do
+    resources :reviews, only: [:index, :show]
   end
 
 end
