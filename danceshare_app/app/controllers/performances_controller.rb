b26f84f952dc81d6bf8d305dd9d06b42296471b7
John Hinrichs <jmhinric@gmail.com>
2014-02-18 14:25:39 -0500

    Refactor create performance

--- danceshare_app/app/controllers/performances_controller.rb
+++ danceshare_app/app/controllers/performances_controller.rb
@@ -2,7 +2,7 @@ class PerformancesController < ApplicationController
 
   before_action :load_company, only: [:index, :show, :new, :create]
   before_action :load_performance, only: [:show, :update, :destroy]
-  before_action :authenticate, only: []
+  # before_action :authenticate
   before_action :admin_authorize, only: [:new, :create]
 
   def index
@@ -14,17 +14,20 @@ class PerformancesController < ApplicationController
   end
 
   def new
+    @venue = Venue.new
   end
 
   def create
-    @venue = yelp_call(params[:venue_name])
+    yelp_data = yelp_call(params[:venue_name])
+    @venue = create_venue(yelp_data)
+
     @performance = Performance.create(
       title: "#{@company.name}", 
       date: params[:date],
-      time: params[:time]
+      time: params[:time],
+      pretty_date: params[:date].to_date.strftime("%A, %B %e, %Y"),
+      pretty_time: params[:time].to_datetime.strftime("%l:%M %p")
       )
-    @performance.pretty_date = @performance.date.strftime("%A, %B %e, %Y")
-    @performance.pretty_time = @performance.time.strftime("%l:%M %p")
       
     @venue.performances << @performance  
     @company.performances << @performance
@@ -62,10 +65,10 @@ class PerformancesController < ApplicationController
       
       yelp_search = access_token.get(path).body
 
-      yelp = JSON(yelp_search)
+      return JSON(yelp_search)
 
       # binding.pry
-      return create_venue(yelp)
+      # return create_venue(yelp)
     end
 
     def create_venue(yelp)
