b9258e3091c08d54731148c5ce7133b5148f17a2
John Hinrichs <jmhinric@gmail.com>
2014-04-04 11:37:32 -0400

    change user new

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   
-  validates :first_name, :last_name, :email, :gender, :dob, presence: true
+  validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
   has_secure_password
   
