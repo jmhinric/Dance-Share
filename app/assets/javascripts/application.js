918618cca303214c29818ee7ba5805528807e401
John Hinrichs <jmhinric@gmail.com>
2014-04-26 19:28:37 -0500

    beginnings of homepage search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,17 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(".header-search form").submit(function(e) {
+  e.preventDefault();
+  console.log("Searched");
+  $.ajax(function(){
+    dataType: "json",
+    url: "/",
+    data: { text: $(".header-search input").val() },
+    success: function(success) {
+      
+    }
+  });
+});
