4ebe1ecee0bd8209dd50edd0d5e0d24341ddde9e
John Hinrichs <jmhinric@gmail.com>
2014-05-12 22:55:22 -0400

    fix test user_creates_company

--- spec/features/user_can_write_reviews.rb
+++ spec/features/user_can_write_reviews.rb
@@ -4,10 +4,11 @@ require 'login_helper'
 describe "user can write reviews of performances" do
   let(:user) { FactoryGirl.create(:user) }
   let(:run) { FactoryGirl.create(:recent_week_run) }
+  let(:company) { FactoryGirl.create(:company) }
 
   it "lets users write reviews of performances" do
     login(user)
-    expect(page).to have_content run.performance.title
+    
 
     click_link "Write a Review"
     
--- spec/features/user_creates_company.rb
+++ spec/features/user_creates_company.rb
@@ -3,32 +3,34 @@ require 'login_helper'
 
 describe "user creates a dance company" do
   let(:user) { FactoryGirl.create(:user) }
-  let!(:company) { FactoryGirl.create(:company, admin: user)}
+  let!(:company) { FactoryGirl.create(:company)}
 
   it "allows a User to create a company" do
     login(user)
     click_link "#{user.first_name}'s Profile"
     expect(page).to have_content user.first_name
 
-    click_link "Create a Dance Company"
-    fill_in "Name", with: "Super Strongs"
-    fill_in "Photo url", with: "http://www.example.com"
+    click_link "Create a Company"
+    fill_in :company_name, with: "Super Strongs"
+    # fill_in "Photo url", with: "http://www.example.com"
     click_button "Create Company"
     expect(page).to have_content "Super Strongs"
   end
 
   it "allows the User to edit the Company profile" do
     user.companies << company
+    company.admin = user
+    company.save
     login(user)
     click_link "#{user.first_name}'s Profile"
     click_link company.name
-    expect(page).to have_content "#{company.name} Profile"
+    expect(page).to have_content "#{company.name}"
 
-    click_link "Edit Profile"
-    fill_in "Name", with: "New Company"
-    click_button "Update Profile"
-    expect(page).to have_content "New Company Profile"
     save_and_open_page
+    click_link "edit"
+    fill_in :company_name, with: "New Company"
+    click_button "Update Profile"
+    expect(page).to have_content "New Company"
   end
   
 end
