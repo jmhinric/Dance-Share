373b483f1c5941acb5ac1aac8dd60f96ee7b82de
John Hinrichs <jmhinric@gmail.com>
2014-05-14 14:38:46 -0400

    fix splash page test

--- spec/features/splash_page_shows_runs.rb
+++ spec/features/splash_page_shows_runs.rb
@@ -6,18 +6,27 @@ describe "splash page shows runs" do
   let!(:run3) { FactoryGirl.create(:upcoming_week_run) }
   let!(:run4) { FactoryGirl.create(:upcoming_month_run) }
 
+  it "displays performances for the upcoming week" do
+    
+    visit root_path
+    within ".home-upcoming" do
+      expect(page).to have_content "Upcoming"
+    
+      expect(page).to have_content run3.company.name
+      expect(page).to have_content run3.performances.order(:date).first.date.strftime("%A, %B %e, %Y")
+      expect(page).to have_content run3.performances.order(:date).last.date.strftime("%A, %B %e, %Y")
+    end
+  end
+
   it "displays performances from the last week" do
     
     visit root_path
     within ".home-recent" do
       expect(page).to have_content "Recent"
     
-      within "div.home-show-info" do
-        expect(page).to have_content run1.company.name
-        # expect(page).to have_content run1.venue.name
-        expect(page).to have_content run1.performances.order(:date).first.date.strftime("%A, %B %e, %Y")
-        expect(page).to have_content run1.performances.order(:date).last.date.strftime("%A, %B %e, %Y")
-      end
+      expect(page).to have_content run1.company.name
+      expect(page).to have_content run1.performances.order(:date).first.date.strftime("%A, %B %e, %Y")
+      expect(page).to have_content run1.performances.order(:date).last.date.strftime("%A, %B %e, %Y")
     end
   end
 
@@ -27,27 +36,9 @@ describe "splash page shows runs" do
     within ".home-recent" do
       expect(page).to have_content "Recent"
     
-      # within "div.home-show-info" do
-        expect(page).to_not have_content run2.company.name
-        # expect(page).to_not have_content run2.venue.name
-        expect(page).to_not have_content run2.performances.order(:date).first.date.strftime("%A, %B %e, %Y")
-        expect(page).to_not have_content run2.performances.order(:date).last.date.strftime("%A, %B %e, %Y")
-      # end
-    end
-  end
-
-  it "displays performances for the upcoming week" do
-    
-    visit root_path
-    within ".home-upcoming" do
-      expect(page).to have_content "Upcoming"
-    
-      within "div.home-show-info" do
-        expect(page).to have_content run3.company.name
-        # expect(page).to have_content run3.venue.name
-        expect(page).to have_content run3.performances.order(:date).first.date.strftime("%A, %B %e, %Y")
-        expect(page).to have_content run3.performances.order(:date).last.date.strftime("%A, %B %e, %Y")
-      end
+      expect(page).to_not have_content run2.company.name
+      expect(page).to_not have_content run2.performances.order(:date).first.date.strftime("%A, %B %e, %Y")
+      expect(page).to_not have_content run2.performances.order(:date).last.date.strftime("%A, %B %e, %Y")
     end
   end
 
@@ -57,12 +48,9 @@ describe "splash page shows runs" do
     within ".home-upcoming" do
       expect(page).to have_content "Upcoming"
     
-      # within "div.home-show-info" do
-        expect(page).to_not have_content run4.company.name
-        # expect(page).to_not have_content run4.venue.name
-        expect(page).to_not have_content run4.performances.order(:date).first.date.strftime("%A, %B %e, %Y")
-        expect(page).to_not have_content run4.performances.order(:date).last.date.strftime("%A, %B %e, %Y")
-      # end
+      expect(page).to_not have_content run4.company.name
+      expect(page).to_not have_content run4.performances.order(:date).first.date.strftime("%A, %B %e, %Y")
+      expect(page).to_not have_content run4.performances.order(:date).last.date.strftime("%A, %B %e, %Y")
     end
   end
 
